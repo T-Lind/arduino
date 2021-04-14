b6166ab882884fc24bf61562bb1e4127bbf59658
T-Lind <82475207+T-Lind@users.noreply.github.com>
2021-04-14 09:35:30 -0700

    Update simple_move_robot.ino

--- simple_move_robot.ino
+++ simple_move_robot.ino
@@ -2,21 +2,22 @@
 #include <Adafruit_PWMServoDriver.h>
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-int pus(double power, bool direction){
-  if(direction == true){
-    return (990*power + 1510);
-  }
+//determine the pulse length in microseconds from a percent power(+ is forward,, - is backward)
+int pus(double power){
+  if(power > 0){
+    return (990*power + 1510); }//something like 0.50, meaning 50% power forwads
+  if(power == 0){
+    return 1500; }//0 power, meaning STOP
   else{
-    return (-990*power + 1490);
-  }
+    return (-990*abs(power) + 1490); }//some negative power is the only other option, so it generates a power in the negative band with the absolute value of the power.
 }
 
-int conversion(double power, bool direction){
-  double pus = 0;
-  int pticks
-  pus = pus(power,direction);
-  phz = 1000000/pus
-  pticks = int((4096*phz)/1600)
+int conversion(double power){
+  double pus = 0;//create variable pulse, in microseconds(us)
+  int pticks = 0;//create pticks variable, in ticks out of 4096.
+  pus = pus(power);//use the pus function to find the proportial pulse length in us from a percent
+  phz = 1000000/pus//convert to hertz
+  pticks = int((4096*phz)/1600)//convert hertz to ticks
   return pticks;
 }
 
@@ -28,9 +29,9 @@ void setup() {
   pwm.setPWMFreq(1600);  // This is the maximum PWM frequency  
   Wire.setClock(400000);
   
-  pwm.setPWM(4,0,conversion(0.50,true));
+  pwm.setPWM(4,0,conversion(0.50));//start motor at 50% power forward
   delay(1000)
-  pwm.setPWM(4,0,4096);
+  pwm.setPWM(4,0,4096);//stop motor
 }
 
 void loop() {
